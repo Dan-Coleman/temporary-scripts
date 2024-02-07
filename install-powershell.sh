b819c5e42f48eb9e33ac1ba21fe02540c757faac
Dan Coleman <dan.coleman@numatic.co.uk>
2024-02-07 14:24:03 +0000

    update script

--- install-powershell.sh
+++ install-powershell.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+###################################
+# Powershell Install
+###################################
+
 # Update the list of packages
 sudo apt-get update
 
@@ -24,3 +28,28 @@ sudo apt-get update
 ###################################
 # Install PowerShell
 sudo apt-get install -y powershell
+
+###################################
+# Azure CLI Install
+###################################
+
+# Get packages needed for the installation process:
+sudo apt-get update
+
+sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
+
+# Download and install the Microsoft signing key
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
+    gpg --dearmor |
+    sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+
+# Add the Azure CLI software repository
+AZ_DIST=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_DIST main" |
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+# Update repository information and install the azure-cli package
+sudo apt-get update
+sudo apt-get install azure-cli
