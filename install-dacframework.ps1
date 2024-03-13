d3328c6b00f4c5ad181a1d9316fc3cb61da985ce
Dan Coleman <dan.coleman@numatic.co.uk>
2024-03-13 16:48:06 +0000

    Update install-dacframework.ps1

--- install-dacframework.ps1
+++ install-dacframework.ps1
@@ -1,6 +1,6 @@
 $link = "https://download.microsoft.com/download/8/c/a/8caaa4ff-fcd6-4b3f-8fa7-5a7c6fa6ea78/x64/DacFramework.msi"
 $soft_name = 'DacFramework.msi'
-$dest = "C:\temp\$soft_name"
+$dest = "C:\Windows\temp\$soft_name"
 
 echo "Downloading DacFramework.msi"
 Invoke-WebRequest -Uri $link -OutFile $dest
