8fb21ef032c842acce3e591a7ecfcc7fbbb786a9
Dan Coleman <dan.coleman@numatic.co.uk>
2024-03-13 16:37:31 +0000

    Update install-dacframework.ps1

--- install-dacframework.ps1
+++ install-dacframework.ps1
@@ -1,22 +1,31 @@
 $link = "https://download.microsoft.com/download/8/c/a/8caaa4ff-fcd6-4b3f-8fa7-5a7c6fa6ea78/x64/DacFramework.msi"
 $soft_name = 'DacFramework.msi'
+$dest = "C:\temp\$soft_name"
 
-$find = Get-WmiObject -Class Win32_Product -Filter "Name = `'$soft_name`'"
+echo "Downloading DacFramework.msi"
+Invoke-WebRequest -Uri $link -OutFile $dest
 
-if ($find -eq $null) {
+echo "Installing msi silenty"
 
-    $tmp = "$env:TEMP\$file"
-    $client = New-Object System.Net.WebClient
-    $client.DownloadFile($link, $tmp)
-
-    msiexec /i $tmp /qn
-    del $tmp
-    echo "Tried installing $soft_name"
-
-} else {
-    echo "ERROR: $soft_name is already installed."
-    echo $find
-    exit 1
+$params = @{
+	"FilePath" = "$Env:SystemRoot\system32\msiexec.exe"
+	"ArgumentList" = @(
+	"/i"
+	"$dest"
+	"/qn"
+	)
+	"Verb" = "runas"
+	"PassThru" = $true
 }
+
+$install = Start-Process @params
+
+$install.WaitForExit()
+
+#$job = Start-Job {[msiexec /i $dest /qn]}
+
+#Wait-Job $job
+
+echo "Tried installing $soft_name"
 
 exit 0
